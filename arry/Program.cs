fb5d56be3861250ecb9aa96ec1467fc92aca8dc2
Geeta Kamble <geetk0501@gmail.com>
2024-03-22 14:55:10 +0530

    funtion

---
+++ arry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hii!");
+        }
+    }
+}
---
+++ update/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace update
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(Math.Min(x,y));
+        }
+    }
+}
